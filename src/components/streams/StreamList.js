63121090881ec413316cc73d14e852ff77064aee
devYeonjun <alcls0026@gmail.com>
2022-05-12 08:57:57 +1200

    Add stream form

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -13,8 +13,10 @@ class StreamList extends React.Component {
 		if (stream.userId === this.props.currentUserId) {
 			return (
 				<div className="right floated content">
-					<Link to={`/streams/edit/${stream.id}`} className="ui button primary">Edit</Link>
-					<Link className="ui button negative">Delete</Link>
+					<Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+						Edit
+					</Link>
+					<button className="ui button negative">Delete</button>
 				</div>
 			);
 		}
